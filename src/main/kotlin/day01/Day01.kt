17f93b5d5eeef2bed49ee5b1acf2d94cbbea8b9d
Manuel Casariego <manuel.casariego@klarna.com>
2023-12-01 11:30:22 +0100

    Changed day 1 to an awesome solution

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -11,45 +11,36 @@ class Day01(private val input: String) : Day() {
     }
 
     override fun part2(): Int {
-        return input.lines().sumOf { findFirst(it) * 10 + findLast(it) }
+
+        return input.lines().sumOf { line ->
+            val first = stringToInt(Regex("(1|one|2|two|3|three|4|four|5|five|6|six|7|seven|8|eight|9|nine).*?").find(line)!!.value)
+            val last = stringToInt(Regex("(1|eno|2|owt|3|eerht|4|ruof|5|evif|6|xis|7|neves|8|thgie|9|enin).*?").find(line.reversed())!!.value)
+            first * 10 + last
+        }
     }
 
-    private fun findFirst(input: String): Int {
-        val auxMap = mutableMapOf<Int, Int>()
-        listOf(
-            "(1|one)",
-            "(2|two)",
-            "(3|three)",
-            "(4|four)",
-            "(5|five)",
-            "(6|six)",
-            "(7|seven)",
-            "(8|eight)",
-            "(9|nine)"
-        ).forEach {
-            auxMap[Regex(it).findAll(input).map { matchResult -> matchResult.range.first }.firstOrNull()
-                ?: Int.MAX_VALUE] = it.toCharArray()[1].digitToInt()
+    private fun stringToInt(input: String): Int{
+        return when (input) {
+            "one" -> 1
+            "two" -> 2
+            "three" -> 3
+            "four" -> 4
+            "five" -> 5
+            "six" -> 6
+            "seven" -> 7
+            "eight" -> 8
+            "nine" -> 9
+            "eno"-> 1
+            "owt" -> 2
+            "eerht" -> 3
+            "ruof" -> 4
+            "evif" -> 5
+            "xis" -> 6
+            "neves" -> 7
+            "thgie" -> 8
+            "enin" -> 9
+            else -> input.toInt()
         }
-        return auxMap.minBy { it.key }.value
-    }
-
-    private fun findLast(input: String): Int {
-        val auxMap = mutableMapOf<Int, Int>()
-        listOf(
-            "(1|one)",
-            "(2|two)",
-            "(3|three)",
-            "(4|four)",
-            "(5|five)",
-            "(6|six)",
-            "(7|seven)",
-            "(8|eight)",
-            "(9|nine)"
-        ).forEach {
-            auxMap[Regex(it).findAll(input).map { matchResult -> matchResult.range.first }.lastOrNull()
-                ?: Int.MIN_VALUE] = it.toCharArray()[1].digitToInt()
-        }
-        return auxMap.maxBy { it.key }.value
     }
 
 }
