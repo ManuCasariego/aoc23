949cf59d5564e24394f4fbdb97661a22376e49b2
manuel.casariego <manucasariego@gmail.com>
2023-12-03 10:17:37 +0100

    extracted common logic in day 3

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -5,14 +5,10 @@ import Day
 class Day03(private val input: String) : Day() {
 
     override fun part1(): Int {
-        val board: List<List<Char>> = input.lines().map { line ->
-            // transform line to list of chars
-            line.toCharArray().toList()
-        }
-        var sum = 0
+        val board: List<List<Char>> = input.lines().map { it.toCharArray().toList() }
         // iterate and get numbers
-        input.lines().forEachIndexed { y, line ->
-            Regex("\\d+").findAll(line).forEach { matchResult ->
+        return input.lines().mapIndexed { y, line ->
+            Regex("\\d+").findAll(line).map { matchResult ->
                 val number = matchResult.value.toInt()
                 // now we have to check if the number is adjacent to any symbol other than period (.)
                 // we can check the 8 adjacent squares per digit
@@ -20,13 +16,9 @@ class Day03(private val input: String) : Day() {
                 val isValid = matchResult.range.any { range ->
                     isSymbolAdjacentToPosition(board, range, y)
                 }
-                if (isValid) sum += number
-            }
-
-
-        }
-        return sum
-
+                if (isValid) number else 0
+            }.sum()
+        }.sum()
     }
 
     override fun part2(): Int {
@@ -37,8 +29,8 @@ class Day03(private val input: String) : Day() {
         }
         // iterate and get numbers
 
-        val auxMap = mutableMapOf<Pair<Int, Int>, List<Triple<Int, Int, Int>>>()
-        // the triple in auxmap will include x, y and the actual number
+        val auxMap = mutableMapOf<Pair<Int, Int>, Set<Triple<Int, Int, Int>>>()
+        // the triple in auxmap will include the actual number, x and y
         input.lines().forEachIndexed { y, line ->
             Regex("\\d+").findAll(line).forEach { matchResult ->
                 val number = matchResult.value.toInt()
@@ -48,53 +40,34 @@ class Day03(private val input: String) : Day() {
                 matchResult.range.forEach { rangeX ->
                     if (isSymbolAdjacentToPosition(board, rangeX, y)) {
                         whatSymbolIsAdjacentToPosition(board, rangeX, y).forEach { triple ->
-                            // tre triples are the symbol, x and y, x and y need to be accessed the other way around
+                            // tre triples are the symbol, x and y
                             if (triple.first == '*') auxMap[Pair(triple.second, triple.third)] =
                                 auxMap[Pair(triple.second, triple.third)]?.plus(
                                     Triple(
+                                        number,
                                         matchResult.range.first,
-                                        y,
-                                        number
+                                        y
                                     )
-                                ) ?: listOf(Triple(matchResult.range.first, y, number))
-
+                                ) ?: setOf(Triple(number, matchResult.range.first, y))
                         }
                     }
                 }
-
-
             }
-
-
         }
         // to check
-        val a = auxMap.map { (_, list) ->
-            if (list.distinct().size == 2) {
-                list.distinct()[0].third * list.distinct()[1].third
-            } else {
-                0
-            }
-        }
-        return a.sum()
-
+        return auxMap.values.map { set ->
+            if (set.size == 2) set.fold(1) { a, b -> a * b.first } else 0
+        }.sum()
     }
 
     private fun isSymbolAdjacentToPosition(board: List<List<Char>>, x: Int, y: Int): Boolean {
-        // calculate all 8 adjacent positions
-        val positions = (-1..1).flatMap { dy ->
-            (-1..1).map { dx ->
-                Pair(
-                    (x + dx).coerceAtLeast(0).coerceAtMost(board[0].size - 1),
-                    (y + dy).coerceAtLeast(0).coerceAtMost(board.size - 1)
-                )
-            }
-        }.distinct()
+        val positions = getAllAdjacentPositions(board, x, y)
         return positions.map { (board[it.second][it.first].isSymbol()) }.any { it }
     }
 
-    private fun whatSymbolIsAdjacentToPosition(board: List<List<Char>>, x: Int, y: Int): List<Triple<Char, Int, Int>> {
+    private fun getAllAdjacentPositions(board: List<List<Char>>, x: Int, y: Int): List<Pair<Int, Int>> {
         // calculate all 8 adjacent positions
-        val positions = (-1..1).flatMap { dy ->
+        return (-1..1).flatMap { dy ->
             (-1..1).map { dx ->
                 Pair(
                     (x + dx).coerceAtLeast(0).coerceAtMost(board[0].size - 1),
@@ -102,6 +75,11 @@ class Day03(private val input: String) : Day() {
                 )
             }
         }.distinct()
+    }
+
+    private fun whatSymbolIsAdjacentToPosition(board: List<List<Char>>, x: Int, y: Int): List<Triple<Char, Int, Int>> {
+        // I'm returning a triplet with the first being the adjacent char, and then the coordinates of it
+        val positions = getAllAdjacentPositions(board, x, y)
         return positions.filter { (board[it.second][it.first].isSymbol()) }
             .map { Triple(board[it.second][it.first], it.first, it.second) }
     }
