0e5444b64cd2f656ec078f872ed8557e5aa971a7
Manuel Casariego <manuel.casariego@klarna.com>
2023-12-04 06:52:45 +0100

    Day 4

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -1,14 +1,54 @@
 package day04
 
 import Day
+import kotlin.math.pow
 
 class Day04(private val input: String) : Day() {
     override fun part1(): Int {
-        return 0
+        val a = input.lines().map { line ->
+            val numberOfMatches = getNumberOfMatches(line)
+            if (numberOfMatches == 0) 0 else 2f.pow(numberOfMatches - 1).toInt()
+        }
+        return a.sum()
+    }
+
+    private fun getNumberOfMatches(line: String): Int {
+        var split = line.split(" ")
+//            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        split = split.dropWhile { it == "Card" || it.matches(Regex("(\\d)+:")) || it == "" }
+
+        val firstNumbers = mutableListOf<Int>()
+        val secondNumbers = mutableListOf<Int>()
+        var foundPipe = false
+        for (elem in split) {
+            if (elem == "|") {
+                foundPipe = true
+                continue
+            }
+            if (elem == "") {
+                continue
+            }
+            if (!foundPipe) {
+                firstNumbers.add(elem.toInt())
+            } else {
+                secondNumbers.add(elem.toInt())
+            }
+        }
+
+        return secondNumbers.filter { it in firstNumbers }.size
     }
 
     override fun part2(): Int {
-        return 0
+        val scratchCardCopies = input.lines().map { 1 }.toMutableList()
+
+        input.lines().forEachIndexed { index, line ->
+            val numberOfMatches = getNumberOfMatches(line)
+
+            (1..numberOfMatches).forEach { dx ->
+                if (index + dx < scratchCardCopies.size) scratchCardCopies[index + dx] += scratchCardCopies[index]
+            }
+        }
+        return scratchCardCopies.sum()
     }
 
 }
--- src/test/kotlin/day04/Day04Test.kt
+++ src/test/kotlin/day04/Day04Test.kt
@@ -12,14 +12,14 @@ class Day04Test : DayTest() {
     @Test
     fun testSolvePart1WithExampleInput() {
         val part1 = solvePart1(exampleDay)
-        val expectedSolution = 0
+        val expectedSolution = 13
         Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
     }
 
     @Test
     fun testSolvePart2WithExampleInput() {
         val part2 = solvePart2(exampleDay)
-        val expectedSolution = 0
+        val expectedSolution = 30
         Assertions.assertEquals(expectedSolution, part2, "Part 1 with example input should equal $expectedSolution")
 
     }
