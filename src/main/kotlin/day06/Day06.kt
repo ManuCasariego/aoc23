bd6eb8d3369855c2fc043a11d425b2965e170771
manuel.casariego <manucasariego@gmail.com>
2023-12-06 21:23:20 +0100

    modified day 6 input handling

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -8,13 +8,11 @@ class Day06(private val input: String) : Day() {
         //Distance:  9  40  200
         // three races, first one last 7ms record distance is 9 mm
         // starting speed 0 mm/ms
-        val times = input.lines().first().substringAfter(":").longs()
-        val records = input.lines().last().substringAfter(":").longs()
-
-        return records.mapIndexed() { index, record ->
-            val time = times[index]
-            getNumberOfCombinationsThatBeatTheRecord(time, record)
-        }.fold(1L) { acc, i -> acc * i }.toLong()
+        return input.lines().first().substringAfter(":").longs()
+            .zip(input.lines().last().substringAfter(":").longs())
+            .map { (time, record) ->
+                getNumberOfCombinationsThatBeatTheRecord(time, record)
+            }.reduce(Long::times)
     }
 
     private fun String.longs() = this.split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
@@ -29,8 +27,8 @@ class Day06(private val input: String) : Day() {
     }
 
     override fun part2(): Long {
-        val time = input.lines().first().substringAfter(":").replace(" ", "").toLong()
-        val record = input.lines().last().substringAfter(":").replace(" ", "").toLong()
+        val time = input.lines().first().substringAfter(":").filter { it.isDigit() }.toLong()
+        val record = input.lines().last().substringAfter(":").filter { it.isDigit() }.toLong()
 
         return getNumberOfCombinationsThatBeatTheRecord(time, record)
     }
