54d6a72f59e6891f4e5e77b26158ffac87d756b2
Manuel Casariego <manuel.casariego@klarna.com>
2023-12-11 07:29:48 +0100

    refactored day 11

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -5,37 +5,6 @@ import kotlin.math.max
 import kotlin.math.min
 
 class Day11(private val input: String) : Day() {
-    override fun part1(): Long {
-        val galaxyList = getGalaxiesList()
-
-        val emptyYs = getEmptyYs()
-        val emptyXs = getEmptyXs()
-
-        // now we need to get every possible distance between the galaxies
-        val galaxiesDiscardSet = mutableSetOf<Pair<Long, Long>>()
-        val distances = galaxyList.map { galaxyA ->
-            galaxiesDiscardSet.add(galaxyA)
-            galaxyList.filter { galaxiesDiscardSet.contains(it) }.sumOf { galaxyB ->
-                val smallX = min(galaxyA.first, galaxyB.first)
-                val largeX = max(galaxyA.first, galaxyB.first)
-                val xDistance = (LongRange(smallX, largeX - 1).count()) +
-                        LongRange(
-                            smallX,
-                            largeX
-                        ).count { it in emptyXs }.toLong()
-                val smallY = min(galaxyA.second, galaxyB.second)
-                val largeY = max(galaxyA.second, galaxyB.second)
-                val yDistance = (LongRange(smallY, largeY - 1).count()) +
-                        LongRange(
-                            smallY,
-                            largeY
-                        ).count { it in emptyYs }.toLong()
-                xDistance + yDistance
-            }
-        }
-
-        return distances.sum()
-    }
 
     private fun getGalaxiesList(): List<Pair<Long, Long>> {
         val galaxiesList = mutableListOf<Pair<Long, Long>>()
@@ -49,38 +18,6 @@ class Day11(private val input: String) : Day() {
         return galaxiesList
     }
 
-
-    override fun part2(): Long {
-        val galaxyList = getGalaxiesList()
-        // now let's get the empty Ys
-        val emptyYs = getEmptyYs()
-        val emptyXs = getEmptyXs()
-
-        // now we need to get every possible distance between the galaxies
-        val galaxiesDiscardSet = mutableSetOf<Pair<Long, Long>>()
-        val distances = galaxyList.map { galaxyA ->
-            galaxiesDiscardSet.add(galaxyA)
-            galaxyList.filter { galaxiesDiscardSet.contains(it) }.sumOf { galaxyB ->
-                val smallX = min(galaxyA.first, galaxyB.first)
-                val largeX = max(galaxyA.first, galaxyB.first)
-                val xDistance = (LongRange(smallX, largeX - 1).count()) +
-                        LongRange(
-                            smallX,
-                            largeX
-                        ).count { it in emptyXs }.toLong() * 999999
-                val smallY = min(galaxyA.second, galaxyB.second)
-                val largeY = max(galaxyA.second, galaxyB.second)
-                val yDistance = (LongRange(smallY, largeY - 1).count()) +
-                        LongRange(
-                            smallY,
-                            largeY
-                        ).count { it in emptyYs }.toLong() * 999999
-                xDistance + yDistance
-            }
-        }
-        return distances.sum()
-    }
-
     private fun getEmptyYs(): List<Long> {
         return input.lines().mapIndexed { y, line ->
             if (!line.contains("#")) y.toLong() else -1
@@ -89,9 +26,48 @@ class Day11(private val input: String) : Day() {
 
 
     private fun getEmptyXs(): List<Long> {
-        return (0..<input.lines()[0].length).map { x ->
-            val numberOfStartsInX = input.lines().map { it[x] }.count { it == '#' }
-            if (numberOfStartsInX == 0) x.toLong() else -1L
+        return (input.lines()[0].indices).map { x ->
+            val numberOfStarsInX = input.lines().count { it[x] == '#' }
+            if (numberOfStarsInX == 0) x.toLong() else -1L
         }.filter { it != -1L }
     }
+
+    private fun getDistances(valueOfEmpty: Int): List<Long> {
+        val galaxyList = getGalaxiesList()
+
+        val emptyYs = getEmptyYs()
+        val emptyXs = getEmptyXs()
+
+        // now we need to get every possible distance between the galaxies
+        val galaxiesDiscardSet = mutableSetOf<Pair<Long, Long>>()
+        return galaxyList.map { galaxyA ->
+            galaxiesDiscardSet.add(galaxyA)
+            galaxyList.filterNot { it in galaxiesDiscardSet }.sumOf { galaxyB ->
+                val smallX = min(galaxyA.first, galaxyB.first)
+                val largeX = max(galaxyA.first, galaxyB.first)
+                val xDistance = (LongRange(smallX, largeX - 1).count()) +
+                        LongRange(
+                            smallX,
+                            largeX
+                        ).count { it in emptyXs }.toLong() * (valueOfEmpty - 1)
+                val smallY = min(galaxyA.second, galaxyB.second)
+                val largeY = max(galaxyA.second, galaxyB.second)
+                val yDistance = (LongRange(smallY, largeY - 1).count()) +
+                        LongRange(
+                            smallY,
+                            largeY
+                        ).count { it in emptyYs }.toLong() * (valueOfEmpty - 1)
+                xDistance + yDistance
+            }
+        }
+    }
+
+    override fun part1(): Long {
+        return getDistances(2).sum()
+    }
+
+    override fun part2(): Long {
+        return getDistances(1000000).sum()
+    }
+
 }
