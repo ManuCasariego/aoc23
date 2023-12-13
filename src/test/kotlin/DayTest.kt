7a7d562691aa2cf4290d09906c9fba4380220161
manuel.casariego <manucasariego@gmail.com>
2023-12-13 16:09:10 +0100

    Removed empty input files, added function that uses your aoc cookie to download the file for you

--- src/test/kotlin/DayTest.kt
+++ src/test/kotlin/DayTest.kt
@@ -1,4 +1,10 @@
 import java.io.File
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
 open class DayTest {
 
@@ -17,10 +23,29 @@ open class DayTest {
     }
 
     protected fun String.readInput(): String {
-        return File("src/test/kotlin/day${this}/input").readText()
+        val path = "src/test/kotlin/day${this}/input"
+        if (!File(path).exists()) {
+            downloadFileIfNotExists("https://adventofcode.com/2023/day/${this}/input", path)
+        }
+        return File(path).readText().trim()
+    }
+
+
+    private fun downloadFileIfNotExists(url: String, localPath: String) {
+        val file = File(localPath)
+        if (!file.exists()) {
+            val sessionCookie = File("src/test/kotlin/sessionCookie.txt").readText().trim()
+            val connection = URL(url).openConnection() as HttpURLConnection
+            connection.setRequestProperty("Cookie", "session=$sessionCookie")
+            val input: InputStream = connection.inputStream
+            Files.copy(input, Paths.get(localPath), StandardCopyOption.REPLACE_EXISTING)
+            println("File downloaded and saved to $localPath")
+        } else {
+            println("File already exists at $localPath")
+        }
     }
 
     protected fun String.readTestInput(): String {
-        return File("src/test/kotlin/day${this}/testInput").readText()
+        return File("src/test/kotlin/day${this}/testInput").readText().trim()
     }
 }
