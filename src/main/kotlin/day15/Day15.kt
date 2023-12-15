24a3fc4e6a38bfcf5fad85b1ec24a82519183a17
Manuel Casariego <manuel.casariego@klarna.com>
2023-12-15 08:24:47 +0100

    day 15

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -4,10 +4,57 @@ import Day
 
 class Day15(private val input: String) : Day() {
     override fun part1(): Long {
-        return 0
+        val a = input.split(',').map { word ->
+            hash(word)
+        }
+        return a.sum().toLong()
+    }
+
+    private fun hash(s: String): Int {
+        var value = 0
+        for (c in s) {
+            value += c.code
+            value *= 17
+            value %= 256
+        }
+        return value
     }
 
     override fun part2(): Long {
-        return 0
+        val instructions = input.split(",")
+        val boxes = List<MutableList<Lens>>(256) { mutableListOf() }
+
+        instructions.forEach { instruction ->
+            val label = if (instruction.contains("=")) instruction.substringBefore("=") else instruction.substringBefore("-")
+            val boxIndex = hash(label)
+            val operation = instruction.substringAfterLast(label)
+            if (operation.startsWith("-")) {
+                // operation to remove
+                boxes[boxIndex].removeIf { it.label == label }
+            } else if (operation.startsWith("=")) {
+                // operation to set
+                val focalLength = operation.substringAfter("=").toInt()
+                val lensIndex = boxes[boxIndex].indexOfFirst { it.label == label }
+                if (lensIndex == -1) {
+                    // not in the box yet
+                    boxes[boxIndex].add(Lens(label, focalLength))
+                } else {
+                    // already in the box
+                    boxes[boxIndex][lensIndex].focalLength = focalLength
+                }
+            }
+        }
+
+        var totalFocusingPower = 0
+        for (i in boxes.indices) {
+            for (j in boxes[i].indices) {
+                totalFocusingPower += (i + 1) * (j + 1) * boxes[i][j].focalLength
+            }
+        }
+        return totalFocusingPower.toLong()
     }
+
+
+    data class Lens(val label: String, var focalLength: Int)
+
 }
--- src/test/kotlin/day15/Day15Test.kt
+++ src/test/kotlin/day15/Day15Test.kt
@@ -1,18 +1,26 @@
 package day15
 
 import DayTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 
 class Day15Test : DayTest() {
     private val day = Day15("15".readInput())
+    private val exampleDay = Day15("15".readTestInput())
 
     @Test
     fun testSolvePart1WithExampleInput() {
+        val part1 = solvePart1(exampleDay)
+        val expectedSolution = 1320L
+        Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
     }
 
     @Test
     fun testSolvePart2WithExampleInput() {
+        val part2 = solvePart2(exampleDay)
+        val expectedSolution = 145L
+        Assertions.assertEquals(expectedSolution, part2, "Part 2 with example input should equal $expectedSolution")
     }
 
     @Test
